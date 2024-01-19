655e58951b086cb5aba53d02e3820a3dd6d5ebcd
maeddes <maeddes@gmail.com>
2024-01-19 08:50:10 +0000

    updates

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,8 +16,10 @@ public class DemoApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Hey");
-	}
 
+		System.out.println("Hey");
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+	
 }
