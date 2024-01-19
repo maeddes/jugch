9f9377cf3ece5c579263ed21852c5410af0d9414
maeddes <maeddes@gmail.com>
2024-01-19 08:49:12 +0000

    updates

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,29 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	@GetMapping
+	String sayHi(){
+
+		return "hi";
+	}
+
+	@PostMapping
+	String test(@PathVariable String x){
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hey");
 	}
 
 }
